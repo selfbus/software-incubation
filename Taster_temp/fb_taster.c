e59201c727e591f13f2493ea45f0401040f1cd6f
Stefan Haller <stefanhaller.sverige@gmail.com>
2014-07-19 16:57:28 +0200

    Taster mit Temperatursensor: Initial checkin

---
+++ Taster_temp/fb_taster.c
@@ -0,0 +1,284 @@
+/*
+ *    _____ ______ __   __________  __  _______ *
+ *   / ___// ____// /  / ____/ __ )/ / / / ___/ *
+ *   \__ \/ __/  / /  / /__ / __  / / / /\__ \  *
+ *  ___/ / /__  / /__/ /__// /_/ / /_/ /___/ /  *
+ * /____/_____//____/_/   /_____/\____//____/   *
+ *
+ *  Copyright (c) 2014, Stefan Haller
+ *  Copyright (c) 2008,2009,2010 Andreas Krebs <kubi@krebsworld.de>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ */
+/**
+* @file   fb_taster.c
+* @author Andreas Krebs <kubi@krebsworld.de>
+* @date   2009
+*
+* @brief The Freebus Taster Application, Firmware fuer einen 4-fach Taster mit 4 LEDs
+*		Herstellercode 0x0004 = Jung
+*		Devicetype 0x1052 = Jung Tastsensor 2092
+*		in ETS das Applikationsprogramm "Universal/Schalten 105201" auswaehlen
+*
+* \par Changes:
+*		1.00	erste Version;
+*		1.01	eigenes Telegramm wird ausgewertet, "Affengriff" entprellt
+* 		1.02	div. bugs behoben
+* 		1.03	Fehler bei Lamellenverstellzeit (T2) behoben
+* 		1.04	LEDs dimmbar, neue backendsoft, fehlende Funktionen zugefügt,
+* 				trimbar über RS
+* 		1.05	neue LIB
+* 		1.06	Anpassung für LPC936, Lib 1.5, Cleanup
+*/
+
+// Options
+#define debugmode
+#define SENSOR_TYPE     0   // !=1 DS18B20
+
+//#define NOPROGLED //typ 0,2 Die Progled blinkt im Progmodus da sie auch Betriebs LED ist
+//#define NOPROGBUTTON	//typ 1,3 es ist kein prog Taster vorhanden sondern progmode wird durch druecken von taste 1&3 oder 2&4 aktiviert
+
+// Geräteparameter setzen, diese können von der ETS übschrieben werden
+// Daher zusätzlich bei jedem restart_app neu schreiben
+//static __code unsigned char __at (0x3903) manufacturer[2]={0x00,0x04};  // Herstellercode 0x0004 = Jung
+//static __code unsigned char __at (0x3905) device_type[2]={0x10, 0x52};  // 0x1052 = Jung Tastsensor 2092
+//static __code unsigned char __at (0x390C) port_A_direction={0};         // PORT A Direction Bit Setting
+//static __code unsigned char __at (0x390D) run_state={255};              // Run-Status (00=stop FF=run)
+
+// DEBUG
+// Temperatur Sensor
+    unsigned char sequence;
+    int th;
+    int temp;
+
+
+#include "fb_app_taster.h"
+#include "watchdog.h"
+#ifdef debugmode
+	#include  "debug.h"
+#endif
+#include "fb_rs232.h"
+#include "onewire.h"
+//#include "rc5.h"
+
+
+#ifdef NOPROGBUTTON
+	#ifdef NOPROGLED
+		#define TYPE 3
+	#else
+		#define TYPE 1
+	#endif
+#else
+	#ifdef NOPROGLED
+		#define TYPE 2
+	#else
+		#define TYPE 0
+	#endif
+#endif
+
+#define VERSION		106
+
+#ifdef DEBUG_H_
+	DEBUG_VARIABLES;
+#endif
+//unsigned char __at (0x00) RAM[00];
+unsigned char object_value[12];	// wird hier deklariert um den Speicher besser auszunutzen!!!
+
+
+/**
+* The start point of the program, init all libraries, start the bus interface, the application
+* and check the status of the program button.
+*
+* @return
+*/
+void main(void)
+{
+	unsigned char n,LED,cmd,tasterpegel=0;
+	__bit blink, verstell, verstellt,tastergetoggelt=0;
+	signed char cal,buttonpattern=1;
+	static __code signed char __at (USERRAM_ADDR_H + 0xBF) trimsave;
+//	static __code unsigned char __at (WRITE_ADDR+ 0xBE) LED_hell;
+
+	/*// Temperatur Sensor
+	unsigned char sequence;
+    int th;
+    int temp;
+*/
+	// Verions bit 6 und 7 für die varianten, bit 0-5 für die verionen (63)
+	//Varianten sind hier noprogbutton=0x040, noprogled=0x80
+	__bit wduf;
+	wduf=WDCON&0x02;
+
+	restart_hw();							// Hardware zuruecksetzen
+	// TODO, sequence in restart_app verschieben
+	sequence=1;
+
+#ifdef NOPROGBUTTON
+	if((((PORT & 0x0F)== 0x03) || ((PORT & 0x0F)== 0x0C)) && wduf) cal=0;
+	else cal=trimsave;
+
+#else
+	TASTER=1;
+	if(!TASTER && wduf)cal=0;
+	else cal=trimsave;
+#endif
+	TRIM = (TRIM+trimsave);
+	TRIM &= 0x3F;				//oberen 2 bits ausblenden
+	WATCHDOG_INIT
+	WATCHDOG_START
+	TASTER=0;
+	for (n=0;n<50;n++) {
+		TR0=0;					// Timer 0 anhalten
+		TH0=eeprom[ADDRTAB+1];	// Timer 0 setzen mit phys. Adr. damit Geräte unterschiedlich beginnen zu senden
+		TL0=eeprom[ADDRTAB+2];
+		TF0=0;					// Überlauf-Flag zurücksetzen
+		TR0=1;					// Timer 0 starten
+		while(!TF0);
+	}
+	restart_app();				// Anwendungsspezifische Einstellungen zuruecksetzen
+#ifdef debugmode
+	DEBUG_SETUP
+#endif
+
+	for (n=0;n<4;n++) switch_led(n,0);	// Alle LEDs abschalten
+
+	LED=0;
+	verstellt=0;
+	// Default LED Helligkeit holen
+	//dimmwert = LED_hell;
+	dimmwert =  eeprom[0xD1];	// Neue VD setzt Defaultwert in EEPROM
+
+	do  {
+		WATCHDOG_FEED
+		if (RTCCON>=0x80) delay_timer();	// Realtime clock ueberlauf
+
+		n=timer;
+		blink=((n>>5) & 0x01);
+		verstell=((n>>2) & 0x01);
+
+		if (verstell==0)verstellt=0;
+
+		if (status60 & 0x01){			//wenn progmode aktiv ist...
+			if ((PORT & 0x0F)==0x0E){	// Taste 1 gedrück
+				if ((dimmwert<254) && (verstell==1)&& verstellt==0){
+					dimmwert++;
+					verstellt=1;
+				}
+			}
+			if ((PORT & 0x0F)==0x0D){ // Taste 2 gedrückt
+				if ((dimmwert>1) && (verstell==1)&& verstellt==0){
+					dimmwert--;
+					verstellt=1;
+					}
+			}
+		}
+		else{	//Wenn also Modul nicht im Progmode ist..
+			//##### TASTERABFRAGE ######
+
+			if(APPLICATION_RUN)	{// nur wenn im Run modus und nicht connected
+				if ((PORT & 0x0F) != button_buffer) port_changed(PORT & 0x0F);	// ein Taster wurde gedrueckt
+
+				// Temperatur verarbeiten
+	                if (sequence==1)
+	                {
+	                    interrupted=0;
+	                    start_tempconversion();         // Konvertierung starten
+	                    if (!interrupted) sequence=2;
+	                }
+	                else if (sequence==2)
+	                {
+	                    if (ow_read_bit()) sequence=3;  // Konvertierung abgeschlossen
+	                }
+	                else
+	                {
+	                    interrupted=0;
+	                    // Temperatur einlesen + Übergabe Sensortyp
+	                    th=read_temp(SENSOR_TYPE);
+	                    if (!interrupted)
+	                    {
+	                        // Bei Sensorfehler wird letzter Messwert gehalten TODO Fehler Com-Objekt einfügen??
+	                        temp=th;
+	                        sequence=1; // TODO, wenn wir hier sind haben wir einen gültigen Messwert
+	                    }
+	                }
+			    }
+		    }
+
+		if (tel_arrived || tel_sent) {//
+			tel_sent=0;
+			process_tel();
+		}
+		else {
+			for(n=0;n<100;n++);
+		}
+
+#ifdef debugmode
+		cmd;
+		DEBUG_POINT
+#endif
+#ifndef NOPROGBUTTON
+		TASTER=1;				        	// Pin als Eingang schalten um Programmiertaster abzufragen
+		if(!TASTER){ // Taster gedrückt
+			if(tasterpegel<255)	tasterpegel++;
+			else{
+				if(!tastergetoggelt)status60^=0x81;	// Prog-Bit und Parity-Bit im system_state toggeln
+				tastergetoggelt=1;
+					if((status60 & 0x01)==0){	//wenn ausgemacht, Dimmwert speichern
+						EA=0;
+						START_WRITECYCLE;
+						FMADRH= EEPROM_ADDR_H;    // Write to EEPROM area above USERRAM
+						//FMADRL= 0xBE;
+						FMADRL= 0xD1;
+						FMDATA=	dimmwert;
+						STOP_WRITECYCLE;
+						EA=1;
+					}
+			}
+		}
+		else {
+			if(tasterpegel>0) tasterpegel--;
+			else tastergetoggelt=0;
+			}
+
+#else
+		// progmode wird durch Taste 1&2 bzw. 3&4 getoggelt
+		if (((PORT & 0x0F)== 0x03) || ((PORT & 0x0F)== 0x0C)) {
+			if(tasterpegel<255)	tasterpegel++;
+			else{
+				if(!tastergetoggelt)status60^=0x81;	// Prog-Bit und Parity-Bit im system_state toggeln
+				tastergetoggelt=1;
+				if((status60 & 0x01)==0){	//wenn ausgemacht Dimmwert speichern
+					EA=0;
+					START_WRITECYCLE;
+					FMADRH= WRITE_ADDR+0x01;    // Write to EEPROM area above USERRAM
+					//FMADRL= 0xBE;
+					FMADRL= 0xD1;
+					FMDATA=	dimmwert;
+					STOP_WRITECYCLE;
+					EA=1;
+				}
+
+			}
+		}
+
+		if ((PORT & 0x0F)== 0x0F){ ;	// Warten bis alle Taster losgelassen
+			if(tasterpegel>0) tasterpegel--;
+			else tastergetoggelt=0;
+			}
+#endif
+
+#ifdef NOPROGLED
+		if (status60 & 0x01) TASTER = blink;		// LED blinkt im Prog-Mode
+		else TASTER = !((eeprom[0xCD] & 0x10) >> 4);	// LED ist an oder aus gemaess Parameter fuer Betriebs-LED
+#else
+		if (status60 & 0x01) TASTER = 0;		// LED leuchtet im Prog-Mode
+		else TASTER = 1;						// LED aus
+#endif
+
+
+	}  while(1);
+}
+
---
+++ Taster_temp/onewire.c
@@ -0,0 +1,192 @@
+/*
+ *    _____ ______ __   __________  __  _______
+ *   / ___// ____// /  / ____/ __ )/ / / / ___/
+ *   \__ \/ __/  / /  / /__ / __  / / / /\__ \
+ *  ___/ / /__  / /__/ /__// /_/ / /_/ /___/ /
+ * /____/_____//____/_/   /_____/\____//____/
+ *
+ *  Copyright (c) 2010 Jan Wegner
+ *  Copyright (c) 2014 Stefan Haller
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ */
+
+
+#include "fb_app_taster.h"
+#include "onewire.h"
+
+
+#define OWDATA 	P2_7	// Data-Pin für one-wire Sensor
+const unsigned char bitmask_1[8] ={0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80};
+
+unsigned char t;
+
+unsigned char bit_count, wait_count;
+
+#define UCHAR unsigned char
+
+// one-wire Gerät initialisieren
+//owdelay(2500);    Hardware
+//owdelay(210);     Software
+
+#define mow_init( mPX_X, mPre, n) \
+{ \
+   mPX_X=0; \
+   owdelay(210); \
+   mPX_X=1; \
+   for(n=0;n<255;n++) mPre=mPre & mPX_X; \
+}
+
+// Byte an one-wire Gerät senden
+// LSB first
+// 1-Bit: 5us auf low dann auf high und 85us warten //260
+// 0-Bit: 90us auf low dann auf high                //275
+#define mow_write( mPX_X, n, m) \
+{ \
+  for (n=1;n!=0;n=n<<1) { \
+      if (owbyte & n) { \
+          for (m=0;m<2;m++) mPX_X=0; \
+              mPX_X=1; \
+              owdelay(23); } \
+      else { \
+          mPX_X=0; \
+          owdelay(25); \
+          mPX_X=1; } \
+  } \
+}
+
+// Byte von one-wire Gerät lesen    //140
+#define mow_read( mPX_X, n, m, d) \
+{ \
+  for (n=1;n!=0;n=n<<1) { \
+      for (m=0;m<2;m++) mPX_X=0; \
+          mPX_X=1; \
+          owdelay(1); \
+          if (mPX_X) d|=n; \
+          owdelay(13); } \
+}
+
+// Bit von one-wire Datenleitung einlesen
+#define mow_read_bit( mPX_X, m, b) \
+{ \
+  for (m=0;m<2;m++) mPX_X=0; \
+      mPX_X=1; \
+      owdelay(1); \
+      b=mPX_X; \
+}
+
+
+__bit ow_init(void)		// one-wire Gerät initialisieren
+{
+  __bit presence;
+  unsigned char n;
+  presence=1;
+
+  mow_init( OWDATA, presence, n);
+  presence=!presence;
+  return (presence);
+}
+
+
+void ow_write(unsigned char owbyte) // Byte an one-wire Gerät senden
+{
+  unsigned char n,m;
+
+  mow_write( OWDATA, n, m);
+}
+
+
+unsigned char ow_read(void)			// Byte von one-wire Gerät lesen
+{
+  unsigned char n,m,d;
+
+  d=0;
+  mow_read( OWDATA, n, m, d);
+  return(d);
+}
+
+
+__bit ow_read_bit(void)				// Bit von one-wire Datenleitung einlesen
+{
+	unsigned char m;
+	__bit b;
+
+	mow_read_bit( OWDATA, m, b);
+    owdelay (13);
+	return (b);
+}
+
+
+void start_tempconversion(void)	// Temperaturmessung starten
+{
+	if (ow_init()) {
+		ow_write(0xCC);			// Skip-ROM command: alle parallel angeschlossenen Geräte werden angesprochen
+		ow_write(0x44);			// start single temperature conversion command
+	}
+}
+
+
+
+signed int read_temp(unsigned char sensortyp)   // Temperatur einlesen
+{
+	unsigned char lsb,msb;
+	signed int t;
+	unsigned int t2;
+	unsigned char sign;
+	unsigned char counts;
+
+
+  	if (ow_init()) {
+  		ow_write(0xCC);			// Skip-ROM command: alle parallel angeschlossenen Geräte werden angesprochen
+  		ow_write(0xBE);			// read scratchpad command: Speicher einlesen
+  		lsb=ow_read();			// LSB von Temperaturwert
+  		msb=ow_read();			// MSB von Temperaturwert
+
+
+		if(sensortyp==1){		// DS18S20
+			counts=ow_read();
+			counts=ow_read();
+			counts=ow_read();
+			counts=ow_read();
+			counts=ow_read();	// counts remaining
+
+			msb&=0xF8;			// oberen 3 Bits von LSB in untere 3 von MSB
+			msb+=(lsb>>5)&0x07;
+
+			lsb=lsb<<3;			// LSB um 3 Bit nach oben verschieben
+			lsb&=0xF0;			// alles unterhalb 2^0 abschneiden
+			lsb+=(16-counts);
+		}
+
+
+		sign=msb&0x80;
+  		t2=msb*256+lsb;
+  		if (sign) t2=(0xFFFF-t2)+1;
+
+
+
+  		t=t2*6;
+  		t2=t2>>2;
+  		t=t+t2;					// hier ist t die Teperatur in 0,01°C
+
+
+		if(sensortyp==1){		// DS18S20
+			t-=25;
+		}
+
+
+  		if (sign) t=-t;			// Vorzeichen
+
+  	}
+  	else t=0xFFFF;				// im Fehlerfall 0xFFFF zurückmelden
+  	return (t);
+}
+
+
+static void owdelay(unsigned char deltime)      // Warten
+{                               // Der Aufruf allein dauert schon ca. 12us !!!
+    for(;deltime>0;deltime--) {}
+}
